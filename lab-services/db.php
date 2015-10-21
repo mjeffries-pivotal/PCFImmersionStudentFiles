334fc2896d5267af006484b587fd702fc10be838
Paul Chapman <chapman@zemsys.com>
2015-10-21 13:42:14 +1100

    db.php for services lab

---
+++ lab-services/db.php
@@ -0,0 +1,29 @@
+<?php
+function getSampleDataFromDB()
+{
+
+//verify that mysqli has been enabled
+    if (function_exists('mysqli_connect') == false) {
+        echo "need to enable mysqli!" ;
+        error_log("need to enable mysqli!", 0);
+        return;
+    }
+
+//connect with hard-coded connection info- look under VCAP_SERVICES
+    $dbname = "YOUR INFO HERE";  //under 'name' and something like: ad_a4fffdc3d80e473
+    $hostname = "YOUR INFO HERE"; // something like us-cdbr-iron-east-02.cleardb.net
+    $username = 'YOUR INFO HERE'; // something like bdfe1eb555af52
+    $password = "YOUR INFO HERE"; // something like 348d05d5
+
+//connect to the database
+    $mysql = mysqli_connect($hostname, $username, $password, $dbname );
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        error_log("Failed to connect to MySQL: " . mysqli_connect_error());
+        return;
+    } else {
+        echo "Success connecting to the db!";
+    }
+}
+?>
+
